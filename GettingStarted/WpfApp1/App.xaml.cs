0a422184fce4368018b8406f96f9be91da95fcdf
Michael Ponti <mike.ponti@honeywell.com>
2019-12-19 14:06:11 -0800

    task: add sample app for getting started

---
+++ GettingStarted/WpfApp1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : PrismApplication
+	{
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+			containerRegistry.Register<Services.ICustomerStore, Services.DbCustomerStore>();
+		}
+
+		protected override Window CreateShell()
+		{
+			var w = Container.Resolve<MainWindow>();
+			return w;
+		}
+	}
+}
